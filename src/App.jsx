0a430329e72a9452fb0e6a3c14fc320324bd0337
Swetanshu <swetanshusingh17@gmail.com>
2023-10-26 14:28:10 +0530

    added inital files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-7xl text-green-400 border-black border-2' >Hello</h1>
+     <h1 className='text-7xl'>Hello</h1>
+    </>
+  )
+}
+
+export default App
